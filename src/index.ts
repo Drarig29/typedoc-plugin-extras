ba47dde2bda42f88e719ceb27d901452f00dbd3b
Corentin Girard <corentingirard.dev@gmail.com>
2020-05-10 03:40:08 +0200

    Copy icon at end of rendering

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Application } from 'typedoc';
 import { PluginHost } from 'typedoc/dist/lib/utils';
 import { ExtrasPlugin } from "./plugin";
+import { copyFileSync } from 'fs';
+import { join } from 'path';
+import { RendererEvent } from 'typedoc/dist/lib/output/events';
 
 export function load(host: PluginHost) {
     const app: Application = host.application;
@@ -13,4 +16,15 @@ export function load(host: PluginHost) {
     // });
 
     app.renderer.addComponent('extras', new ExtrasPlugin(app.renderer));
+
+    app.renderer.once(RendererEvent.END, () => {
+        const workingDir = process.cwd();
+        const outDir = app.options.getValue('out') || './docs';
+        const icon = 'favicon.ico';
+
+        const from = join(workingDir, icon);
+        const to = join(workingDir, outDir, icon);
+
+        copyFileSync(from, to);
+    });
 }
