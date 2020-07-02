324c7e8f15c243533888094591de261e213d24f3
Corentin Girard <corentingirard.dev@gmail.com>
2020-07-02 16:23:22 +0200

    Refactored in a new helpers file

---
+++ src/helpers.ts
@@ -0,0 +1,43 @@
+/**
+ * Creates a relative path from a host file to a target file which is located in the root.
+ * @example `modules/_index_.html` --> `../favicon.ico`
+ * @param hostPath The path of the file which will contain the resulting relative path.
+ * @param targetFilename The name of the target file.
+ */
+export function makeRelativeToRoot(hostPath: string, targetFilename: string): string {
+    // Find separators.
+    const match = hostPath.match(/[/\\]/g);
+    if (!match) return targetFilename;
+
+    // Create path with one '../' per separator and targetFilename at the end.
+    const separatorCount = match.length;
+    const parts = [...Array(separatorCount).fill('..'), targetFilename];
+    return parts.join('/');
+}
+
+/**
+ * Appends a favicon link at the end of `<head>` in the given html.
+ * @param html HTML string to append to.
+ * @param url URL of the favicon.
+ */
+export function appendFavicon(html: string, url: string): string {
+    return html.replace('</head>',
+        '\t' + // Some tabulation to fit the rest of the document.
+        `<link rel="icon" href="${url}" />` +
+        '\n' + // Push the end of <head> to the next line.
+        '</head>'
+    );
+}
+
+/**
+ * Appends date and/or time after "Generated using TypeDoc".
+ * @param html HTML string to append to.
+ * @param dateTime A string containing date and/or time.
+ */
+export function appendDateTime(html: string, dateTime: string): string {
+    return html.replace(/(<p>Generated using.*TypeDoc.*)(<\/p>)/,
+        '$1' + // Start of <p>
+        dateTime +
+        '$2' // End of <p>
+    );
+}
