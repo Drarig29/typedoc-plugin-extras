573c8a3bd156e326e739076eca5535e2f47c177a
Corentin Girard <corentingirard.dev@gmail.com>
2020-07-02 16:23:22 +0200

    Refactored in a new helpers file

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,23 +1,7 @@
+import { makeRelativeToRoot, appendFavicon, appendDateTime } from './helpers';
 import { RendererComponent } from 'typedoc/dist/lib/output/components';
 import { PageEvent } from 'typedoc/dist/lib/output/events';
 import { basename } from 'path';
-
-/**
- * Creates a relative path from a host file to a target file which is located in the root.
- * @example `modules/_index_.html` --> `../favicon.ico`
- * @param hostPath The path of the file which will contain the resulting relative path.
- * @param targetFilename The name of the target file.
- */
-function makeRelativeToRoot(hostPath: string, targetFilename: string): string {
-    // Find separators.
-    const match = hostPath.match(/[/\\]/g);
-    if (!match) return targetFilename;
-
-    // Create path with one '../' per separator and targetFilename at the end.
-    const separatorCount = match.length;
-    const parts = [...Array(separatorCount).fill('..'), targetFilename];
-    return parts.join('/');
-}
 
 export class ExtrasPlugin extends RendererComponent {
 
@@ -34,11 +18,7 @@ export class ExtrasPlugin extends RendererComponent {
         // Add icon.
         if (!noFavicon) {
             const faviconUrl = makeRelativeToRoot(page.url, favicon);
-            page.contents = page.contents.replace('</head>',
-                '\t' +
-                `<link rel="icon" href="${faviconUrl}" />` +
-                '\n' +
-                '</head>');
+            page.contents = appendFavicon(page.contents, faviconUrl);
         }
 
         // Add generation date and/or time.
@@ -47,14 +27,11 @@ export class ExtrasPlugin extends RendererComponent {
             const date = ` the ${now.toLocaleDateString()}`
             const time = ` at ${now.toLocaleTimeString()}`;
 
-            let appended = ',';
-            if (!hideDate) appended += date;
-            if (!hideTime) appended += time;
+            let dateTime = ',';
+            if (!hideDate) dateTime += date;
+            if (!hideTime) dateTime += time;
 
-            page.contents = page.contents.replace(/(<p>Generated using.*TypeDoc.*)(<\/p>)/,
-                '$1' +      // Start of <p>
-                appended +  // Date and/or time
-                '$2');      // End of <p>
+            page.contents = appendDateTime(page.contents, dateTime);
         }
     }
 }
